3c79aeac5c45d2014266a909b35994359301f974
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-05-23 15:13:44 -0400

    templateNewVisitor.js agregando formulario de visitante

--- src/assets/views/templateNewVisitor.js
+++ src/assets/views/templateNewVisitor.js
@@ -1,7 +1,22 @@
 export const templateNewVisitor = () => {
     //creamos div que contendrá la plantilla
     const containerNewVisitor = document.createElement('div');
-    const contentNewVisitor = `<div><button id="newvisitor" onclick="window.location.href='#/Finish'">formulario</button></div>`
+    const contentNewVisitor = `    <img src="assets/img/LobbyLogo.png" width="150px" alt="logo">
+    <div><button id="newvisitor" onclick="window.location.href='#/Finish'">formulario</button></div>
+    <p><strong>Nuevo visitante</strong></p>
+    <p>Nombre y Apellido</p>
+        <input type="text" class="lastname" id="name" name="">
+        <p>Fecha de llegada </p>
+        <input id="date" type="date">
+        <p>Hora de llegada </p>
+        <input type="time" id="hour" name="">
+        <p>Rut </p>
+        <input type="text" id="rut" name="">
+        <form action="../../form-result.php" method="post" enctype="multipart/form-data" target="_blank">
+          <p>        
+            Seleccionar foto (opcional) </p>     
+            <input type="file" name="archivosubido">                
+        </form>`
     //pasar cel contenido al div
     containerNewVisitor.innerHTML = contentNewVisitor;
     //le pido que busque el id del botón dentro del div cerrado
